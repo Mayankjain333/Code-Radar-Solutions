19a4d355e489365a078e2306ef6a8ea6c4506f9b
Mayankjain333 <Mayankjainofficial222@gmail.com>
2025-02-13 13:13:37 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int a , b;
+    char op;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%c",&op);
+    int add = a+b;
+    int subtract = a-b;
+    int multitply = a*b;
+    int division = a/b;
+    if(op=='+'){
+        printf("%d",add);
+    }
+    else if(op=='-'){
+        printf("%d",subtract);
+    }
+    else if(op=='*'){
+        printf("%d",multitply);
+    }
+    else if(op=='/'){
+        printf("%d",division);
+    }
+    else{
+        printf("error");
+    }
+    return 0;
+}
